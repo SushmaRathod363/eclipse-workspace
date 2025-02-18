5acc82802904b80f2579d02f96ae470ede9fcb17
User <User@DESKTOP-CCMCD1O>
2025-02-19 00:04:16 +0530

    First commit

---
+++ LearnProgramming/src/com/javaprogramming/pragram2.java
@@ -0,0 +1,48 @@
+package com.javaprogramming;
+
+public class pragram2 {
+	
+	public static void main(String[] args)
+	{
+		/*
+		int a; //declaration
+		a = 10; //assignment
+		*/
+		
+		/*
+		int a= 100; //declaration + assignment
+		System.out.println(a);
+		
+		a = 200;
+		
+		System.out.println(a);
+		*/
+		
+		//Approch 1 - if all the variables belongs to different data types
+//		/*int a = 100;
+		int b = 200;
+		int c = 400;
+		*/
+		
+		//Approch 2 - if all the variables are belongs to same data type
+		/*int a,b,c;
+		a=100;
+		b=200;
+		c=300;
+		*/
+		
+		//Approch 3 - if all the variables are belongs to same data type
+		int a=100,b=200,c=300;
+		System.out.println("The value of a is: " +a);
+		System.out.println("The value of a is: " +b);
+		System.out.println("The value of a is: " +c);
+		
+		System.out.println(a + " "+b+ " "+c);
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ LearnProgramming/src/com/javaprogramming/program1.java
@@ -0,0 +1,20 @@
+package com.javaprogramming;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Programmer..!"); //1 Line command lines
+		
+		//Multiple Line command Lines
+		/*
+		 * Hello, we can comment more the 1 lines using this structure
+		 * System.out.println("Multiple command usage");
+		 */
+		
+		System.out.println("Hy, You're successfully started programming!");
+
+	}
+
+}
