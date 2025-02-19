d82d84c4e5b4542c5dcccff6b697edfa6b106265
User <User@DESKTOP-CCMCD1O>
2025-02-19 22:12:55 +0530

    Commit: Variables and Data types

---
+++ LearnProgramming/src/com/javaprogramming/dataTypes.java
@@ -0,0 +1,45 @@
+package com.javaprogramming;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		// Numeric data types
+		
+		int a=100, b=200;
+		System.out.println("The value of a is: " +a);
+		System.out.println("The value of a is: " +b);
+		
+		System.out.println("The sum of a and b is: " +(a+b));
+		
+		byte ab=125;
+		System.out.println(ab);
+		
+		short xy=8978;
+		System.out.println(xy);
+		
+		long abc = 90000808098L; //Write lower and uper case L/l
+		System.out.println(abc);
+		
+		
+		//Decimal numbers -- float, double
+		
+		float item=15.5F; //write lower and upper F/l literals
+		System.out.println(item);
+		
+		double itemValue = 12.98000;
+		System.out.println(itemValue);
+		
+		char grad='A';
+		System.out.println(grad);
+		
+		String name="Jhon";
+		System.out.println(name);
+		
+		boolean bl = true; // Boolean only contains true and false has input value
+		System.out.println(bl);
+	
+		
+		
+	}
+
+}
